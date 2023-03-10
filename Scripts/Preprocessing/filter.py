e7fee5125e6b2e5b7d8f4a7b213e8c3a55580539
melissafasol <melissaclairefasol@gmail.com>
2023-03-10 09:54:15 +0000

    option to select channel type

--- Scripts/Preprocessing/filter.py
+++ Scripts/Preprocessing/filter.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd 
 from scipy import signal 
 
-from parameters import channel_numbers, channel_types
+from parameters import channel_variables
 
 
 class NoiseFilter:
@@ -13,8 +13,10 @@ class NoiseFilter:
     nyquist = sampling_rate/2
     low = 0.2/nyquist
     high = 100/nyquist
+
     
-    def __init__(self, unfiltered_data, number_of_epochs, noise_limit, brain_state_file, channel_type):
+    def __init__(self, unfiltered_data, number_of_epochs, noise_limit, brain_state_file, channel_variables,
+                 ch_type):
         '''input unfiltered data and the number of epochs data needs to be split into
         - this should be equivalent to the length of a brainstate file. Class will output new brainstate file
         with identified noisy epochs:
@@ -27,7 +29,10 @@ class NoiseFilter:
         self.number_of_epochs = number_of_epochs
         self.noise_limit = noise_limit
         self.brain_state_file = brain_state_file
-        self.channel_type = channel_type
+        self.channel_variables = channel_variables
+        self.channel_types= channel_variables['channel_types']
+        self.channel_numbers = channel_variables['channel_numbers']
+        self.ch_type = ch_type
         
     def find_packetloss_indices(self):
         
@@ -41,23 +46,23 @@ class NoiseFilter:
                 packet_loss_score.append(0) if packet_loss(epoch) == True else packet_loss_score.append(6)
             return packet_loss_score
         
-        def butter_bandpass(self):
+        def butter_bandpass(data):
             butter_b, butter_a = signal.butter(self.order, [self.low, self.high], btype = 'band', analog = False)
-            filtered_data = signal.filtfilt(butter_b, butter_a, self.unfiltered_data)
+            filtered_data = signal.filtfilt(butter_b, butter_a, data)
             return filtered_data
         
-        def filter_data_type(channel_type):
+        def filter_data_type():
             indices = []
-            if channel_type == 'eeg':
-                for idx, ch_type in enumerate(channel_types):
-                    if ch_type == 'eeg':
+            if self.ch_type == 'eeg':
+                for idx, ch in enumerate(self.channel_types):
+                    if ch == 'eeg':
                         indices.append(idx)
-            if channel_type == 'emg':
-                for idx, ch_type in enumerate(channel_types):
-                    if ch_type == 'emg':
+            if self.ch_type == 'emg':
+                for idx, ch in enumerate(self.channel_types):
+                    if ch == 'emg':
                         indices.append(idx)
-            if channel_type == 'all':
-                indices = channel_numbers
+            if self.ch_type == 'all':
+                indices = self.channel_numbers
             return indices
         
         #label noise above 3000mV
@@ -72,11 +77,11 @@ class NoiseFilter:
         self.brain_state_file.loc[noise_indices, 'brainstate'] = 6
         
         #select all channels, only emg channels or only eeg channels 
-        indices = filter_data_type(self.channel_type)
+        indices = filter_data_type()
         selected_channels = self.unfiltered_data[indices, :]
         
         #bandpass filter data       
-        bandpass_filtered_data = butter_bandpass(selected_channels)
+        bandpass_filtered_data = butter_bandpass(data = selected_channels)
         
                 
         return self.brain_state_file, bandpass_filtered_data
