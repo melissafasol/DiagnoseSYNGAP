70ee2c0cf8fb92b3bae479804887bb08cf1af2c0
melissafasol <melissaclairefasol@gmail.com>
2023-03-21 14:07:38 +0000

    reducing repetition with inheritance

--- Scripts/NonLinearAnalysis/Entropy/preprocessing.py
+++ Scripts/NonLinearAnalysis/Entropy/preprocessing.py
@@ -9,33 +9,10 @@ from filter import NoiseFilter
 
 
 class EntropyFilter(NoiseFilter):
-    
+    '''Class inherits from filtering class in Preprocessing folder but changes lower and upper
+    bound of IIR filter for entropy analysis to avoid power-line artefacts'''
     def __init__(self, unfiltered_data, brain_state_file, channelvariable, ch_type):
         super.__init__(unfiltered_data, brain_state_file, channelvariable, ch_type)
+        self.order = 3
         self.low = 0.2/125.2
         self.high = 48/125.2
-    
-    def entropy_bandpass_filter(self):
-        
-        def butter_bandpass(data):
-            butter_b, butter_a = signal.butter(3, [0.2/125.2, 48/125.2], btype= 'band', analog=False)
-            filtered_data = signal.filtfilt(butter_b, butter_a, data)
-            return filtered_data
-        
-        indices = []
-        if self.ch_type == 'eeg':
-            for idx, ch in enumerate(self.channel_types):
-                if ch == 'eeg':
-                        indices.append(idx)
-            if self.ch_type == 'emg':
-                for idx, ch in enumerate(self.channel_types):
-                    if ch == 'emg':
-                        indices.append(idx)
-            if self.ch_type == 'all':
-                indices = self.channel_numbers
-        
-        #Select all, emg, or eeg channel indices to apply bandpass filter                                    
-        selected_channels = self.unfiltered_data[indices, :]     
-        bandpass_filtered_data=butter_bandpass(data=selected_channels) 
-                        
-        return bandpass_filtered_data
