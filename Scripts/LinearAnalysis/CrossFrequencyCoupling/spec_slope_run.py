270e30518a79ad412685961d9e30b08df9045d29
melissafasol <melissaclairefasol@gmail.com>
2023-04-21 14:52:43 +0100

    debug to run spec slope calculations

--- Scripts/LinearAnalysis/CrossFrequencyCoupling/spec_slope_run.py
+++ Scripts/LinearAnalysis/CrossFrequencyCoupling/spec_slope_run.py
@@ -15,15 +15,13 @@ from load_files import LoadFiles
 from filter import NoiseFilter, HarmonicsFilter, remove_seizure_epochs
 from exploratory import FindNoiseThreshold
 from constants import start_time_GRIN2B_baseline, end_time_GRIN2B_baseline, GRIN_het_IDs, GRIN2B_ID_list, GRIN2B_seiz_free_IDs, channel_variables, GRIN_wt_IDs
-from spec_slope_functions import power_calc
+
+from spec_slope_functions import SpectralSlope
 
 directory_path = '/home/melissa/PREPROCESSING/GRIN2B/GRIN2B_numpy'
-
-
+results_path = '/home/melissa/RESULTS/XGBoost/FOOOF/'
 
 for animal in GRIN2B_ID_list:
-    fm = FOOOF()
-    frequency_values = np.arange(0.2, 48.2, 0.2)
     print('loading ' + str(animal))
     animal = str(animal)
     load_files = LoadFiles(directory_path, animal)
@@ -35,7 +33,19 @@ for animal in GRIN2B_ID_list:
     bandpass_filtered_data_1 = noise_filter_1.filter_data_type()
     bandpass_filtered_data_2 = noise_filter_2.filter_data_type()
     print('data filtered')
-    power_arr_1 = power_calc(bandpass_filtered_data_1)
-    power_arr_2 = power_calc(bandpass_filtered_data_2)
+    chan2_filter_1 = np.split(bandpass_filtered_data_1[2], 17280, axis = 0)
+    chan2_filter_2 = np.split(bandpass_filtered_data_2[2], 17280, axis = 0)
+    spec_slope_1 = SpectralSlope(chan2_filter_1)
+    spec_slope_2 = SpectralSlope(chan2_filter_2)
+    power_arr_1 = spec_slope_1.power_calc()
+    power_arr_2 = spec_slope_2.power_calc()
+    aperiodic_1 = spec_slope_1.fooof_analysis(power_arr_1)
+    print(str(animal) + 'aperiodic br 1 complete')
+    aperiodic_2 = spec_slope_2.fooof_analysis(power_arr_2)
+    print(str(animal) + ' aperiodic br 2 complete')
+    os.chdir(results_path)
+    np.save(str(animal) + '_BR1.npy', power_arr_1)
+    np.save(str(animal) + '_BR2.npy', power_arr_2)
+
     
     
