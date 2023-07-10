74561954fff74051dfcd9d6959c65eb8192b0e2b
melissafasol <melissaclairefasol@gmail.com>
2023-07-10 11:53:19 +0100

    script to load and plot raw data and save npy array

--- Scripts/Preprocessing/plot_raw_human.py
+++ Scripts/Preprocessing/plot_raw_human.py
@@ -5,13 +5,16 @@ import numpy as np
 import matplotlib
 import mne
 
-
+#read human data 
 os.chdir('/home/melissa/PREPROCESSING/SYNGAP1/SYNGAP1_Human_Data')
-raw_edf = mne.io.read_raw_edf('P1 N1_(1).edf', eog = [0, 1],
+raw_edf = mne.io.read_raw_edf('P1 N1_(1).edf', eog = [0],
             exclude = ['F3:M2', 'C3:M2', 'O1:M2', 'Position', 'PLM1', 'PLM2',
                        'Snore', 'Flow', 'Effort', 'Thorax', 'Abdomen', 'SpO2', 'Pleth', 'Pulse',
                       'E1:M2', 'E2:M2'], preload=True)
 
+
+#extract array object to save
+data, times = raw_edf[:, :]
 
 #extract data as a numpy array to save
 data, times = raw_edf[:]
