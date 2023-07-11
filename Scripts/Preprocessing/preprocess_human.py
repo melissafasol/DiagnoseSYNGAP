704b61f426f8e0681e51be52866632ab68a47bdb
melissafasol <melissaclairefasol@gmail.com>
2023-07-11 11:34:58 +0100

    function to identify noise epochs with power spectrum slope and intercept

--- Scripts/Preprocessing/preprocess_human.py
+++ Scripts/Preprocessing/preprocess_human.py
@@ -35,3 +35,26 @@ epochs = split_into_epochs(data = data, sampling_rate = 256, num_seconds = 30)
 
 
 
+def identify_noisy_epochs(split_epochs, num_channels, num_epochs):
+    channels_idx = list(np.arange(0, num_channels))
+    epochs_idx = list(np.arange(0, num_epochs))
+    
+    intercept_noise = []
+    slope_noise = []
+    for chan in channels_ind:
+        for epoch in epochs_ind:
+            freq, power = scipy.signal.welch(test_epochs[epoch][chan], window='hann', fs=256, nperseg=7680)
+            slope, intercept = np.polyfit(freq, power, 1)
+            if intercept < 1e-13:
+                int_noise_dict = {'Intercept': [intercept], 'Epoch_IDX': [epoch], 'Channel': [chan]}
+                int_noise_df = pd.DataFrame(data = int_noise_dict)
+                intercept_noise.append(int_noise_df)
+            elif slope > -1e-13:
+                slope_noise_dict = {'Slope': [slope], 'Epoch_IDX': [epoch], 'Channel': [chan]}
+                slope_noise_df = pd.DataFrame(data = slope_noise_dict)
+                slope_noise.append(slope_noise_df)
+            
+    intercept_noise_concat = pd.concat(intercept_noise)
+    slope_noise_concat = pd.concat(slope_noise)
+    
+    return intercept_noise_concat, slope_noise_concat
