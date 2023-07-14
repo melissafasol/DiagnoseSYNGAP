1a4b7a3e0c36371e6f1ce26454c8c301368f7313
melissafasol <melissaclairefasol@gmail.com>
2023-07-14 13:14:56 +0100

    threshold harmonic function

--- Scripts/Preprocessing/preprocess_human.py
+++ Scripts/Preprocessing/preprocess_human.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import mne
 import scipy 
 import seaborn as sns
+import signal 
 
 
 def load_filtered_data(file_path, file_name):
@@ -70,6 +71,33 @@ def remove_duplicate_idx(slope_indices_df, intercept_indices_df):
     
     return rm_dup
 
+
+def thresholding_algo( y, lag, threshold, influence):
+    signals = np.zeros(len(y))
+    filteredY = np.array(y)
+    avgFilter = [0]*len(y)
+    stdFilter = [0]*len(y)
+    avgFilter[lag - 1] = np.mean(y[0:lag])
+    stdFilter[lag - 1] = np.std(y[0:lag])
+    for i in range(lag, len(y)):
+        if abs(y[i] - avgFilter[i-1]) > threshold * stdFilter [i-1]:
+            if y[i] > avgFilter[i-1]:
+                signals[i] = 1
+            else:
+                signals[i] = -1
+                filteredY[i] = influence * y[i] + (1 - influence) * filteredY[i-1]
+                avgFilter[i] = np.mean(filteredY[(i-lag+1):i+1])
+                stdFilter[i] = np.std(filteredY[(i-lag+1):i+1])
+        else:
+            signals[i] = 0
+            filteredY[i] = y[i]
+            avgFilter[i] = np.mean(filteredY[(i-lag+1):i+1])
+            stdFilter[i] = np.std(filteredY[(i-lag+1):i+1])
+
+    signal_calc = np.asarray(signals)
+    harmonic_1 = np.mean(signal_calc[25:50])
+    harmonic_2 = np.mean(signal_calc[60:85])
+    return harmonic_1, harmonic_2
 
 def harmonic_filter(channels_idx, epochs_idx, split_epochs, noise_indices):
     
