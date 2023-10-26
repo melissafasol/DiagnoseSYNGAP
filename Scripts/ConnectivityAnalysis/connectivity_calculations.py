0acef67d725282b063dfd2b42a984f639492ccf1
melissafasol <melissaclairefasol@gmail.com>
2023-10-26 14:31:54 +0100

    fixed bugs in refactored script

--- Scripts/ConnectivityAnalysis/connectivity_calculations.py
+++ Scripts/ConnectivityAnalysis/connectivity_calculations.py
@@ -1,8 +1,9 @@
+import sys
 import os
 import pandas as pd
 import numpy as np 
 
-rom mne_features.univariate import compute_higuchi_fd, compute_hurst_exp
+from mne_features.univariate import compute_higuchi_fd, compute_hurst_exp
 
 sys.path.insert(0, '/home/melissa/PROJECT_DIRECTORIES/EEGFeatureExtraction/Scripts/Preprocessing')
 from load_files import LoadFiles
@@ -13,20 +14,22 @@ from constants import SYNGAP_baseline_start, SYNGAP_baseline_end, channel_variab
 from connectivity_class import ConnectivityClass
 
 directory_path = '/home/melissa/PREPROCESSING/SYNGAP1/numpyformat_baseline'
-results_path = '/home/melissa/RESULTS/XGBoost/SYNGAP1/Connectivity'
+results_path = '/home/melissa/RESULTS/FINAL_MODEL/Rat/Connectivity/'
 error_path = '/home/melissa/RESULTS/XGBoost/SYNGAP1/ConnectivityErrors/'
 
 
 SYNGAP_1_ID_ls = [ 'S7088', 'S7092', 'S7094', 'S7098', 'S7068', 'S7074', 'S7076', 'S7071', 'S7075']
-syngap_2_ls =  ['S7091', 'S7070', 'S7072', 'S7083', 'S7063','S7064', 'S7069', 'S7086', 'S7091'] #S7101
+syngap_2_ls =  ['S7091', 'S7070', 'S7072', 'S7083', 'S7063','S7064', 'S7069', 'S7086', 'S7091', 'S7101'] 
 
-analysis_ls = [ 'S7088', 'S7092', 'S7094', 'S7098', 'S7068', 'S7074', 'S7076', 'S7071', 'S7075']
+analysis_ls = [ 'S7088', 'S7092', 'S7094', 'S7098', 'S7068', 'S7074', 'S7076', 'S7071', 'S7075',
+               'S7091', 'S7070', 'S7072', 'S7083', 'S7063','S7064', 'S7069', 'S7086', 'S7091', 'S7101']
+
 
 connectivity_type = ['cross_corr', 'phase_lock_val'] 
 
 for connectivity_cal in connectivity_type:
     for animal in analysis_ls:
-        print('motor one starting')
+        print(animal)
         print('loading ' + str(animal))
         animal = str(animal)
         load_files = LoadFiles(directory_path, animal)
@@ -42,24 +45,39 @@ for connectivity_cal in connectivity_type:
             filter_2 = np.moveaxis(np.array(np.split(bandpass_filtered_data_2, 17280, axis = 1)), 1, 0)
             complexity_calculations_1 = ConnectivityClass(filter_1)
             complexity_calculations_2 = ConnectivityClass(filter_2)
-            if connectivity_cal == 'cross_cor':
-                max_cross_corr_1, error_1 = complexity_calculations_1.calculate_max_cross_corr()
-                max_cross_corr_2, error_2 = complexity_calculations_2.calculate_max_cross_corr()
+            if connectivity_cal == 'cross_corr':
+                print('cross corr beginning')
+                max_cross_corr_1, error_1 = complexity_calculations_1.calculate_max_cross_corr(num_epochs= 17280)
+                max_cross_corr_2, error_2 = complexity_calculations_2.calculate_max_cross_corr(num_epochs= 17280)
+                max_cross_corr_concat = np.concatenate((max_cross_corr_1, max_cross_corr_2), axis = 0)
+                np.save(results_path + str(animal) + '_' + str(connectivity_cal) + '.npy', max_cross_corr_concat)
+                np.save(results_path + str(animal) + '_' + str(connectivity_cal) + '_error_1.npy', error_1)
+                np.save(results_path + str(animal) + '_' + str(connectivity_cal) + '_error_2.npy', error_2)
+                
             elif connectivity_cal == 'phase_lock_val':
-                phase_lock_val_1, error_1 = complexity_calculations_1.calculate_phase_lock_value()
-                phase_lock_val_2, error_2 = complexity_calculations_2.calculate_phase_lock_value()
+                print('phase lock beginning')
+                phase_lock_val_1, error_1 = complexity_calculations_1.calculate_phase_lock_value(num_epochs= 17280)
+                phase_lock_val_2, error_2 = complexity_calculations_2.calculate_phase_lock_value(num_epochs= 17280)
+                max_phase_lock_concat = np.concatenate((phase_lock_val_1, phase_lock_val_2), axis = 0)
+                np.save(results_path + str(animal) + '_' + str(connectivity_cal) + '.npy', max_phase_lock_concat)
+                np.save(results_path + str(animal) + '_' + str(connectivity_cal) + '_error_1.npy', error_1)
+                np.save(results_path + str(animal) + '_' + str(connectivity_cal) + '_error_2.npy', error_2)
+                
 
         if animal in SYNGAP_1_ID_ls :
+            print(animal + ' one file')
             data_1, brain_state_1 = load_files.load_one_analysis_file(start_times_dict = SYNGAP_baseline_start, end_times_dict = SYNGAP_baseline_end)
             noise_filter_1 = NoiseFilter(data_1, brain_state_file = brain_state_1, channelvariables = channel_variables,ch_type = 'eeg')    
             bandpass_filtered_data_1 = noise_filter_1.filter_data_type()
             filter_1 = np.moveaxis(np.array(np.split(bandpass_filtered_data_1, 17280, axis = 1)), 1, 0)
             complexity_calculations_1 = ConnectivityClass(filter_1)
-            if connectivity_cal == 'cross_cor':
-                max_cross_corr_1, error_1 = complexity_calculations_1.calculate_max_cross_corr()
-                np.save(results_path + str(animal) + '_' + str(connectivity_cal) + '.npy')
-                np.save(error_path + str(animal) + '_' + str(connectivity_cal) + '.npy')
+            if connectivity_cal == 'cross_corr':
+                print('cross corr beginning')
+                max_cross_corr_1, error_1 = complexity_calculations_1.calculate_max_cross_corr(num_epochs= 17280 )
+                np.save(results_path + str(animal) + '_' + str(connectivity_cal) + '.npy', max_cross_corr_1)
+                np.save(results_path + str(animal) + '_' + str(connectivity_cal) + '_error_1.npy', error_1)
             elif connectivity_cal == 'phase_lock_val':
-                phase_lock_val_1, error_1 = complexity_calculations_1.calculate_phase_lock_value()
-                np.save(results_path + str(connectivity_cal) + '/' + str(animal) + '_' + str(connectivity_cal) + '.npy')
-                np.save(error_path + str(connectivity_cal) + '/' + str(animal) + '_' + str(connectivity_cal) + '.npy')
+                print('phase lock beginning')
+                phase_lock_val_1, error_1 = complexity_calculations_1.calculate_phase_lock_value(num_epochs= 17280)
+                np.save(results_path + str(connectivity_cal) + '/' + str(animal) + '_' + str(connectivity_cal) + '.npy', phase_lock_val_1)
+                np.save(results_path + str(connectivity_cal) + '/' + str(animal) + '_' + str(connectivity_cal) + '_error_1.npy', error_1)
