3c6f9c81e1208ae39d567d27ccfe07937fa70e93
melissafasol <melissaclairefasol@gmail.com>
2023-11-18 18:26:26 +0000

    adapt figure to select mne indices

--- Scripts/Human/dispen_human.py
+++ Scripts/Human/dispen_human.py
@@ -11,7 +11,7 @@ import EntropyHub as EH
 from preprocess_human import load_filtered_data, split_into_epochs, select_clean_indices
 
 human_data_folder = '/home/melissa/PREPROCESSING/SYNGAP1/SYNGAP1_Human_Data'
-results_path = '/home/melissa/RESULTS/XGBoost/Human_SYNGAP1/DispEn'
+results_path = '/home/melissa/RESULTS/FINAL_MODEL/Human/Complexity/DispEn_DF/'
 noise_directory = '/home/melissa/PREPROCESSING/SYNGAP1/human_npy/harmonic_idx/'
 
 patient_list  =  ['P1 N1', 'P2 N1', 'P2 N2', 'P3 N1', 'P3 N2', 'P4 N1', 'P4 N2', 'P5 N1',
@@ -20,26 +20,42 @@ patient_list  =  ['P1 N1', 'P2 N1', 'P2 N2', 'P3 N1', 'P3 N2', 'P4 N1', 'P4 N2',
                   'P22 N1','P23 N1', 'P23 N2', 'P23 N3', 'P24 N1','P27 N1','P28 N1',
                   'P28 N2', 'P29 N2', 'P30 N1']  
 
-# Define a function to calculate DispEn for a channel
-def calculate_dispen(patient_id, channel_data, clean_indices):
-    dispen_values = [EH.DispEn(data, m=3, tau=2, c=4, Typex='ncdf')[0] for data in channel_data[clean_indices]]
-    np.save(f"{patient_id}_{channel}_dispen.npy", dispen_values)
 
-# Iterate through patient_list
+def calculate_dispen(data_input, patient_id, channel_list, clean_indices, results_path):
+    
+    all_indices = []
+    
+    for clean_idx in clean_indices:
+        all_channels = []
+        for chan_idx, channel in enumerate(channel_list):
+            dispen_value_chan = EH.DispEn(Sig = np.array(data_input[clean_idx][chan_idx, :]), m=3, tau=2, c=4, Typex='ncdf')
+            dispen_dict = {str(channel) + '_dispen': dispen_value_chan}
+            dispen_df = pd.DataFrame(data = dispen_dict)
+            all_channels.append(dispen_df)
+        dispen_idx = pd.concat(all_channels, axis = 1)
+        all_indices.append(dispen_idx)
+    dispen_all_epochs = pd.concat(all_indices, axis = 0)
+    
+    dispen_all_epochs.to_csv(results_path + str(patient_id) + '_dispen.csv')
+    
+
 for patient in patient_list:
     print(patient)
-    file_name = f"{patient}_(1).edf"
-    filtered_data = load_filtered_data(file_path=human_data_folder, file_name=file_name)
-    number_epochs, epochs = split_into_epochs(filtered_data, sampling_rate=256, num_seconds=30)
-    clean_indices = select_clean_indices(noise_directory=noise_directory, patient_id=patient, total_num_epochs=number_epochs)
-    print('Data loaded')
+    file_name = patient + '_(1).edf'
+    filtered_data = load_filtered_data(file_path = human_data_folder, file_name = file_name)
+    number_epochs, epochs = split_into_epochs(filtered_data, sampling_rate = 256, num_seconds = 30)
+    epoch_array = np.array(epochs)
+    
+    #load mne features
+    patient_mne = pd.read_csv(mne_directory + patient + '_all_conn_measures.csv')
+    mne_indices = patient_mne['Idx'].to_list()
     
     # Calculate DispEn for each channel
     channels = ['E1', 'E2', 'F3', 'C3', 'O1', 'M2']
-    for channel in channels:
-        calculate_dispen(patient, epochs[:, channels.index(channel)], clean_indices)
+ 
+    calculate_dispen(data_input = epochs, patient_id = patient,
+                         channel_list = channels,
+                         clean_indices = mne_indices,
+                         results_path = results_path)
     
     print('All channels calculated')
-
-    os.chdir(results_path)
-    print(f"{patient} saved")
