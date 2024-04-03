9874f667dfbae8044461aeadafeaafbd1c1294df
melissafasol <melissaclairefasol@gmail.com>
2024-04-03 12:14:44 +0100

    modulate script

--- Scripts/Preprocessing/remove_noise_br.py
+++ Scripts/Preprocessing/remove_noise_br.py
@@ -12,94 +12,67 @@ sys.path.insert(0, '/home/melissa/PROJECT_DIRECTORIES/EEGFeatureExtraction/Scrip
 from load_files import LoadFiles
 from filter import NoiseFilter, HarmonicsFilter, remove_seizure_epochs
 from exploratory import FindNoiseThreshold
-from constants import SYNGAP_baseline_start, SYNGAP_baseline_end, channel_variables
+from constants import SYNGAP_baseline_start, SYNGAP_baseline_end, channel_variables, seizure_free_IDs, seizure_two_files
 
 directory_path = '/home/melissa/PREPROCESSING/SYNGAP1/numpyformat_baseline/'
 seizure_br_path = '/home/melissa/PREPROCESSING/SYNGAP1/csv_seizures/'
-
-train_2_ids =  ['S7096'] #['S7070', 'S7072', 'S7083', 'S7063', 'S7064', 'S7069']
-train_1_ids = ['S7101']
-seizure_two_syngap = ['S7063', 'S7064', 'S7069', 'S7072']
-seizure_free_IDs = ['S7070', 'S7083', 'S7098', 'S7101', 'S7096']
+clean_br_path = '/home/melissa/PREPROCESSING/SYNGAP1/clean_br/'
 
 br_number = 0
 
-for animal in train_2_ids:
-    animal = str(animal)
-    print(animal)
-    load_files = LoadFiles(directory_path, animal)
-    data_1, data_2, brain_state_1, brain_state_2 = load_files.load_two_analysis_files(start_times_dict = SYNGAP_baseline_start, end_times_dict = SYNGAP_baseline_end)
-    #print(len(data_1[0]))
-    #print(len(brain_state_1))
-    #print(len(data_2[0]))
-    #print(len(brain_state_2))
-    #data_1, brain_state_1 = load_files.load_one_analysis_file(start_times_dict = SYNGAP_baseline_start, end_times_dict = SYNGAP_baseline_end)
-    print('all files loaded')
-    if animal in seizure_free_IDs:
-        explor_test_1 = FindNoiseThreshold(data = data_1, brain_state_file = brain_state_1,noise_limit = 3000, channelvariables = channel_variables, br_number = br_number)
-        explor_test_2 = FindNoiseThreshold(data = data_2, brain_state_file = brain_state_2, noise_limit = 3000, channelvariables = channel_variables, br_number = br_number)
-        clean_br_1, packet_loss_idx_1 = explor_test_1.find_packetloss_indices()
-        clean_br_2, packet_loss_idx_2 = explor_test_2.find_packetloss_indices()
-        slope_thresh_1, int_thresh_1 = explor_test_1.calc_noise_thresh(packet_loss_idx_1)
-        slope_thresh_2, int_thresh_2 = explor_test_2.calc_noise_thresh(packet_loss_idx_2)
-        noise_filter_1 = NoiseFilter(data_1, brain_state_file = brain_state_1, channelvariables = channel_variables,ch_type = 'eeg')    
-        noise_filter_2 = NoiseFilter(data_2, brain_state_file = brain_state_2, channelvariables = channel_variables,ch_type = 'eeg')    
+def preprocess_data_for_animals(animal_ids, directory_path, seizure_free_ids, seizure_br_path, SYNGAP_baseline_start, SYNGAP_baseline_end, channel_variables, br_number):
+    for animal_id in animal_ids:
+        print(animal_id)
+        load_files = LoadFiles(directory_path, animal_id)
+        data_1, data_2, brain_state_1, brain_state_2 = load_files.load_two_analysis_files(start_times_dict=SYNGAP_baseline_start, end_times_dict=SYNGAP_baseline_end)
+        print('all files loaded')
+
+        # Determine if the animal is seizure-free
+        seizure_free = animal_id in seizure_free_ids
+
+        # Preprocess brain state files based on seizure presence
+        if not seizure_free:
+            print('seizure pipeline beginning')
+            br_1 = pd.read_csv(seizure_br_path + f'{animal_id}_BL1_Seizures.csv')
+            br_2 = pd.read_csv(seizure_br_path + f'{animal_id}_BL2_Seizures.csv')
+            brain_state_1 = remove_seizure_epochs(brain_state_1, br_1)
+            brain_state_2 = remove_seizure_epochs(brain_state_2, br_2)
+            print('removed seizure indices')
+
+        # Common processing for all animals
+        noise_filter_1 = NoiseFilter(data_1, brain_state_file=brain_state_1, channelvariables=channel_variables, ch_type='eeg')
+        noise_filter_2 = NoiseFilter(data_2, brain_state_file=brain_state_2, channelvariables=channel_variables, ch_type='eeg')
         bandpass_filtered_data_1 = noise_filter_1.filter_data_type()
         bandpass_filtered_data_2 = noise_filter_2.filter_data_type()
-        power_calc_full_rec_1, noise_indices_1 = noise_filter_1.power_calc_noise(bandpass_filtered_data_1, slope_thresh = slope_thresh_1, int_thresh = int_thresh_1, clean_br = clean_br_1, br_number = br_number)
-        power_calc_full_rec_2, noise_indices_2 = noise_filter_2.power_calc_noise(bandpass_filtered_data_2, slope_thresh = slope_thresh_2, int_thresh = int_thresh_2, clean_br = clean_br_2, br_number = br_number)
-        
-        
-        os.chdir('/home/melissa/PREPROCESSING/SYNGAP1/clean_br/')
-        clean_br_1.loc[noise_indices_1, 'brainstate'] = 5
-        clean_br_2.loc[noise_indices_2, 'brainstate'] = 5
-        clean_br_1.to_pickle(animal + '_BL1.pkl')
-        clean_br_2.to_pickle(animal + '_BL2.pkl')
-        
-        power_calc_full_rec_1, noise_indices_1 = noise_filter_1.power_calc_noise(bandpass_filtered_data_1, slope_thresh = slope_thresh_1, int_thresh = int_thresh_1, clean_br = clean_br_1, br_number = br_number)
-        power_calc_full_rec_2, noise_indices_2 = noise_filter_2.power_calc_noise(bandpass_filtered_data_2, slope_thresh = slope_thresh_2, int_thresh = int_thresh_2, clean_br = clean_br_2, br_number = br_number)    
-        
-        
-    else:
-        print('seizure pipeline beginning')
-        os.chdir(seizure_br_path)
-        br_1 = pd.read_csv( str(animal) + '_BL1_Seizures.csv')
-        br_2 = pd.read_csv( str(animal) + '_BL2_Seizures.csv')
-        br_no_seizures_1 = remove_seizure_epochs(brain_state_1, br_1)
-        br_no_seizures_2 = remove_seizure_epochs(brain_state_2, br_2)
-        print('removed seizure indices')
-        explor_test_1 = FindNoiseThreshold(data = data_1, brain_state_file = br_no_seizures_1,noise_limit = 3000, channelvariables = channel_variables)
-        explor_test_2 = FindNoiseThreshold(data = data_2, brain_state_file = br_no_seizures_2, noise_limit = 3000, channelvariables = channel_variables)
-        clean_br_1, packet_loss_idx_1 = explor_test_1.find_packetloss_indices()
-        clean_br_2, packet_loss_idx_2 = explor_test_2.find_packetloss_indices()
-        slope_thresh_1, int_thresh_1 = explor_test_1.calc_noise_thresh(packet_loss_idx_1)
-        slope_thresh_2, int_thresh_2 = explor_test_2.calc_noise_thresh(packet_loss_idx_2)
-        noise_filter_1 = NoiseFilter(data_1, brain_state_file = br_no_seizures_1, channelvariables = channel_variables,ch_type = 'eeg')
-        noise_filter_2 = NoiseFilter(data_2, brain_state_file = br_no_seizures_2, channelvariables = channel_variables,ch_type = 'eeg')
-        bandpass_filtered_data_1 = noise_filter_1.filter_data_type()
-        bandpass_filtered_data_2 = noise_filter_2.filter_data_type()
-        power_calc_full_rec_1, noise_indices_1 = noise_filter_1.power_calc_noise(bandpass_filtered_data_1, slope_thresh = slope_thresh_1, int_thresh = int_thresh_1, clean_br = clean_br_1, br_number = br_number)
-        power_calc_full_rec_2, noise_indices_2 = noise_filter_2.power_calc_noise(bandpass_filtered_data_2, slope_thresh = slope_thresh_2, int_thresh = int_thresh_2, clean_br = clean_br_2, br_number = br_number)
-        
-        clean_br_1.loc[noise_indices_1, 'brainstate'] = 5
-        clean_br_2.loc[noise_indices_2, 'brainstate'] = 5
-        
-        print('all noise indices identified')
-        harmonic_int_1 = HarmonicsFilter(filtered_data = bandpass_filtered_data_1, br_state_file = clean_br_1, br_state_num = br_number, noise_array = noise_indices_1)
-        harmonic_indices_1 = harmonic_int_1.harmonics_algo()
-        harmonic_int_2 = HarmonicsFilter(filtered_data = bandpass_filtered_data_2, br_state_file = clean_br_2, br_state_num = br_number, noise_array = noise_indices_2)
-        harmonic_indices_2 = harmonic_int_2.harmonics_algo()
+
+        # Find noise and calculate thresholds
+        processed_data_1 = process_data(noise_filter_1, bandpass_filtered_data_1, br_number)
+        processed_data_2 = process_data(noise_filter_2, bandpass_filtered_data_2, br_number)
+
+        # Save cleaned brain states
+        save_clean_brain_states(clean_br_path, animal_id, processed_data_1['clean_br'], processed_data_2['clean_br'])
+
+def process_data(noise_filter, bandpass_filtered_data, br_number, animal_id, seizure_free_ids):
+    clean_br, packet_loss_idx = noise_filter.find_packetloss_indices()
+    slope_thresh, int_thresh = noise_filter.calc_noise_thresh(packet_loss_idx)
+    power_calc_full_rec, noise_indices = noise_filter.power_calc_noise(bandpass_filtered_data, slope_thresh=slope_thresh, int_thresh=int_thresh, clean_br=clean_br, br_number=br_number)
+
+    # Determine if the animal is seizure-free
+    seizure_free = animal_id in seizure_free_ids
+
+    # Mark noise indices
+    clean_br.loc[noise_indices, 'brainstate'] = 5
+    if not seizure_free:
+        harmonic_indices = HarmonicsFilter(filtered_data=bandpass_filtered_data, br_state_file=clean_br, br_state_num=br_number, noise_array=noise_indices).harmonics_algo()
         print('harmonics analysis run')
+        clean_br.loc[harmonic_indices, 'brainstate'] = 3
+
+    return {'clean_br': clean_br, 'noise_indices': noise_indices}
+
+def save_clean_brain_states(clean_br_path, animal_id, clean_br_1, clean_br_2):
+    clean_br_1.to_pickle(clean_br_path + f'{animal_id}_BL1.pkl')
+    clean_br_2.to_pickle(clean_br_path + f'{animal_id}_BL2.pkl')
+    print('Saved cleaned brain states')
     
-        os.chdir('/home/melissa/PREPROCESSING/SYNGAP1/clean_br/')
-        clean_br_1.loc[noise_indices_1, 'brainstate'] = 5
-        clean_br_2.loc[noise_indices_2, 'brainstate'] = 5
-        clean_br_1.loc[harmonic_indices_1, 'brainstate'] = 3
-        clean_br_2.loc[harmonic_indices_2, 'brainstate'] = 3
-        
-        clean_br_1.to_pickle(animal + '_BL1.pkl')
-        clean_br_2.to_pickle(animal + '_BL2.pkl')
-        
-        #power_calc_full_rec_1, noise_indices_1 = noise_filter_1.power_calc_noise(bandpass_filtered_data_1, slope_thresh = slope_thresh_1, int_thresh = int_thresh_1, clean_br = clean_br_1, br_number = br_number)
-        #power_calc_full_rec_2, noise_indices_2 = noise_filter_2.power_calc_noise(bandpass_filtered_data_2, slope_thresh = slope_thresh_2, int_thresh = int_thresh_2, clean_br = clean_br_2, br_number = br_number)
-        
+    
+preprocess_data_for_animals(animal_id_list, directory_path, seizure_free_IDs, seizure_br_path, SYNGAP_baseline_start, SYNGAP_baseline_end, channel_variables, br_number)
