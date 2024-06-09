40141062ad7060857d3ce7c4c37a57baae5c31d8
melissafasol <melissaclairefasol@gmail.com>
2024-06-09 18:59:54 +0100

    restructured complexity class by channel

--- Scripts/Complexity/complexity_class.py
+++ Scripts/Complexity/complexity_class.py
@@ -1,92 +1,34 @@
-import os 
-import sys
-import numpy as np 
-import pandas as pd 
-import scipy 
-import matplotlib
-import mne 
-
+import numpy as np
+import pandas as pd
 from mne_features.univariate import compute_higuchi_fd, compute_hurst_exp
-
 
 class ComplexClass:
     
-    def __init__(self, complexity_calculation, motor_indices, visual_indices, somatosensory_indices):
+    def __init__(self, complexity_calculation, channel_labels):
         self.complexity_calculation = complexity_calculation
-        self.motor_indices = motor_indices
-        self.visual_indices = visual_indices
-        self.somatosensory_indices = somatosensory_indices
+        self.channel_labels = channel_labels
+        self.num_channels = len(channel_labels)
     
-    def two_recordings(self, bandpass_filtered_data_1, bandpass_filtered_data_2, region):
+    def process_recordings(self, bandpass_filtered_data, concatenated=False):
+        complexity_values = []
+        split_size = 34560 if concatenated else 17280
         
-        region_ls_1 = []
-        region_ls_2 = []
-        
-        if region == 'motor':
-            index_list = self.motor_indices
-        elif region == 'visual':
-            index_list = self.visual_indices
-        elif region == 'somatosensory':
-            index_list = self.somatosensory_indices
-        else:
-            raise ValueError("Invalid region. Please specify 'motor', 'visual', or 'somatosensory'.")
-            
-        for idx in index_list:
-            filter_1 = np.split(bandpass_filtered_data_1[idx], 17280, axis = 0)
-            filter_2 = np.split(bandpass_filtered_data_2[idx], 17280, axis = 0)
+        for idx in range(self.num_channels):
+            filter_data = np.split(bandpass_filtered_data[idx], split_size, axis=0)
             if self.complexity_calculation == 'hurst':
-                hurst_1 = self.calculate_hurst_exp(filter_1)
-                hurst_2 = self.calculate_hurst_exp(filter_2)
-                region_ls_1.append(hurst_1)
-                region_ls_2.append(hurst_2)
+                complexity_values.append(self.calculate_hurst_exp(filter_data))
             elif self.complexity_calculation == 'hfd':
-                hfd_1 = self.calculate_hfd_exp(filter_1)
-                hfd_2 = self.calculate_hfd_exp(filter_2)
-                region_ls_1.append(hfd_1)
-                region_ls_2.append(hfd_2)
+                complexity_values.append(self.calculate_hfd_exp(filter_data))
             else:
                 raise ValueError("Invalid complexity measure")
         
-        mean_array_1 = np.mean(np.array(region_ls_1), axis=0)
-        mean_array_2 = np.mean(np.array(region_ls_2), axis=0)
-        complexity_array = np.concatenate((mean_array_1, mean_array_2), axis=0)
+        complexity_array = np.array(complexity_values)
         return complexity_array
-    
-    def one_recording(self, bandpass_filtered_data_1, region):
-        
-        region_ls_1 = []
-        
-        if region == 'motor':
-            index_list = self.motor_indices
-        elif region == 'visual':
-            index_list = self.visual_indices
-        elif region == 'somatosensory':
-            index_list = self.somatosensory_indices
-        else:
-            raise ValueError("Invalid region. Please specify 'motor', 'visual', or 'somatosensory'.")
-            
-        for idx in index_list:
-            filter_1 = np.split(bandpass_filtered_data_1[idx], 17280, axis = 0)
-            
-            if self.complexity_calculation == 'hurst':
-                hurst_1 = self.calculate_hurst_exp(filter_1)
-                region_ls_1.append(hurst_1)
-            elif self.complexity_calculation == 'hfd':
-                hfd_1 = self.calculate_hfd_exp(filter_1)
-                region_ls_1.append(hfd_1)
-            else:
-                raise ValueError("Invalid complexity measure")
-        
-        mean_array = np.mean(np.array(region_ls_1), axis=0)
-        return mean_array
-       
-        
+
     def calculate_hurst_exp(self, channel_data):
         hurst_values = [compute_hurst_exp(np.expand_dims(epoch, axis=0)) for epoch in channel_data]
-        return hurst_values
+        return np.array(hurst_values).flatten()
 
     def calculate_hfd_exp(self, channel_data):
         hfd_values = [compute_higuchi_fd(np.expand_dims(epoch, axis=0)) for epoch in channel_data]
-        return hfd_values
-
-        
+        return np.array(hfd_values).flatten()
