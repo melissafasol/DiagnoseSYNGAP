e51452317f34d6efef81b8ac953674bbb248d9fe
melissafasol <melissaclairefasol@gmail.com>
2024-07-06 22:38:26 +0100

    hyperopt

--- Scripts/ML/XGBoost/hyperopt.py
+++ Scripts/ML/XGBoost/hyperopt.py
@@ -9,9 +9,9 @@ def hyperparameter_tuning(space: Dict[str, Union[float, int]],
                          X_train: pd.DataFrame, y_train: pd.Series, 
                          X_test: pd.DataFrame, y_test: pd.Series, 
                          early_stopping_rounds: int = 50, 
-                         metric: Callable = accuracy_score) -> Dict[str, Any]:
+                         metric: callable = accuracy_score) -> Dict[str, Any]:
     
-    '''Perform hyperparameter tuning for an XGBoost classifier. 
+    '''Perform hyperparameter runing for an XGBoost classifier. 
     
     This function takes a dictionary of hyperparameters, training and test data, and an optional value
     for early stopping rounds, and returns a dictionary with the loss and model resulting from 
@@ -34,30 +34,27 @@ def hyperparameter_tuning(space: Dict[str, Union[float, int]],
     The test target
     
     early_stopping rounds: int, optional 
-    The number of early stopping rounds to use. The default is 50
+    The number of early stopping rounds to use. The deault is 50
     
     metric: callable
-    Metric to maximize. Default is accuracy
+    Metric to maximise. Default is accuracy
     
     Returns: 
     Dict[str, Any]
         A dictionary with the loss and model resulting from the tuning process. 
         The loss is a float, and the model is an XGBoost classifier'''
     
-    int_vals = ['max_depth', 'n_estimators']
+    int_vals = ['max_depth', 'reg_alpha']
     
     space = {k: (int(val) if k in int_vals else val)
             for k, val in space.items()}
     
+    space['early_stopping_rounds'] = early_stopping_rounds
+    
     model = xgb.XGBClassifier(**space)
     evaluation = [(X_train, y_train), 
-                  (X_test, y_test)]
+                 (X_test, y_test)]
+    model.fit(X_train, y_train, eval_set = evaluation, verbose = False)
     
-    model.fit(X_train, y_train, eval_set=evaluation, 
-              early_stopping_rounds=early_stopping_rounds, 
-              verbose=False)
-    
-    y_pred = model.predict(X_test)
-    score = metric(y_test, y_pred)
-    
+    score = metrics.roc_auc_score(y_test, model.predict(X_test))
     return {'loss': -score, 'status': STATUS_OK, 'model': model}
